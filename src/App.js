a85d54bf12dca40de1aa502268a76ca282523b21
JOSEPH BESKY J <104523855+JOSEPHBESKY@users.noreply.github.com>
2022-08-09 08:42:29 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {addtocart} from './slice'
+ import axios from 'axios'
+function App() {
+  const state=useSelector((state)=>(state))
+  console.log(state);
+  const dispatch=useDispatch()
+  const [task,settask]=useState([])
+  console.log(task);
+  useEffect(()=>{
+    gettask()
+  },[])
+  const gettask=async()=>{
+    // fetch("https://jsonplaceholder.tyicode.com/todos")
+    // .then((res)=> res.json())
+    // .then((json)=>settask(json))
+    // axios.get().then((res)=>settask(res.data))
+    const res=await axios.get("https://jsonplaceholder.typicode.com/posts")
+    console.log(res.data);
+    settask(res.data)
+  }
+  const addcart=()=>{
+    dispatch(addtocart([{name:"joseph"}]))
+  }
+  return (
+    <div>
+    <button onClick={()=>addcart()}>click</button>
+   { task.map((item,index)=><div key={index}><p>{item.tittle}</p></div>)}  </div>
+  )
+}
+
+export default App
---
+++ src/slice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit'
+export const cartSlice=createSlice({
+    name:"cart",
+    initialState:{
+        cartidems:[]
+    },
+    reducers:{
+        addtocart:(state,action)=>{
+            console.log(state,action);
+            state.cartidems=action.payload
+        }
+    }
+})
+
+export const {addtocart}=cartSlice.actions;
+export default cartSlice.reducer;
